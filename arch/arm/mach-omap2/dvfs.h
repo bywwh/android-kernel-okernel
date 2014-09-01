8d3584432fa1b73dd1a6ba5d7243189de88df5bc
ShevT <shev_t@mail.ru>
2014-09-01 19:57:30 +0400

    Revert "OMAP2+: DVFS: provide ability to know if dvfs is scaling for a domain"
    This reverts commit e76fe1ced49fd2d79bdf4501138acec395b5bbcf.

--- arch/arm/mach-omap2/dvfs.h
+++ arch/arm/mach-omap2/dvfs.h
@@ -28,8 +28,6 @@ static inline bool omap_dvfs_is_any_dev_scaling(void)
 {
 	return mutex_is_locked(&omap_dvfs_lock);
 }
-
-bool omap_dvfs_is_scaling(struct voltagedomain *voltdm);
 #else
 static inline int omap_dvfs_register_device(struct device *dev,
 		char *voltdm_name, char *clk_name)
@@ -42,7 +40,6 @@ static inline int omap_device_scale(struct device *req_dev,
 	return -EINVAL;
 }
 static inline bool omap_dvfs_is_any_dev_scaling(void)
-static inline bool omap_dvfs_is_scaling(struct voltagedomain *voltdm)
 {
 	return false;
 }
