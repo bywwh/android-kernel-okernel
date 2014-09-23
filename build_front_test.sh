f5e4621d4d3181b71caa848290b95a9dfaec197f
ShevT <shev_t@mail.ru>
2014-09-23 17:59:38 +0400

    Added AnyKernel zip for any custom rom. Thaks xyyx.

--- build_front_test.sh
+++ build_front_test.sh
@@ -8,6 +8,7 @@
 # 2011-10-24 effectivesky : modified
 # 2010-12-29 allydrop     : created
 ##############################################################################
+
 ##############################################################################
 # set toolchain
 ##############################################################################
@@ -17,29 +18,34 @@ export CROSS_COMPILE=~/AndroidSources/linaro-4.9.2/bin/arm-eabi-
 export LOCALVERSION="-test"
 export CCACHE_DIR=~/.ccache/kernel
 ccache -M 5G
+
 ##############################################################################
 # set variables
 ##############################################################################
 export KERNELDIR=`pwd`
-KERNEL_OUT=$KERNELDIR/obj/KERNEL_OBJ/
+KERNEL_OUT=$KERNELDIR/obj/KERNEL_OBJ
 STRIP=${CROSS_COMPILE}strip
+
 ##############################################################################
 # make zImage
 ##############################################################################
 mkdir -p $KERNEL_OUT
+mkdir -p $KERNEL_OUT/tmp/kernel
+mkdir -p $KERNEL_OUT/tmp/system/lib/modules
+
 make O=$KERNEL_OUT front_test_defconfig
 make -j10 O=$KERNEL_OUT
 
 if [ -f $KERNEL_OUT/arch/arm/boot/zImage ]
 then
     cp -f $KERNEL_OUT/arch/arm/boot/zImage ./
+    mv -f $KERNEL_OUT/arch/arm/boot/zImage $KERNEL_OUT/tmp/kernel/zImage
 fi
 
 ##############################################################################
 # make SGX module
 ##############################################################################
-
-if [ -f $KERNEL_OUT/arch/arm/boot/zImage ]
+if [ -f ./zImage ]
 then
     make clean -C $KERNELDIR/sgx/pvr-source/eurasiacon/build/linux2/omap4430_android
     make -j10 -C $KERNELDIR/sgx/pvr-source/eurasiacon/build/linux2/omap4430_android KERNELDIR=$KERNEL_OUT TARGET_PRODUCT="blaze" BUILD=release TARGET_SGX=540 PLATFORM_VERSION=4.0
@@ -48,4 +54,9 @@ then
     make clean -C $KERNELDIR/sgx/pvr-source/eurasiacon/build/linux2/omap4430_android
     rm -r $KERNELDIR/sgx/pvr-source/eurasiacon/binary2_540_120_omap4430_android_release
     cp $KERNEL_OUT/pvrsrvkm_sgx540_120.ko ./pvrsrvkm_sgx540_120.ko
+    mv $KERNEL_OUT/pvrsrvkm_sgx540_120.ko $KERNEL_OUT/tmp/system/lib/modules/pvrsrvkm_sgx540_120.ko
+    cp ./android/blank_any_kernel.zip new_any_kernel.zip
+    pushd $KERNEL_OUT/tmp
+    zip -r ../../../new_any_kernel.zip ./system/
+    zip -r ../../../new_any_kernel.zip ./kernel/
 fi
--- clean.sh
+++ clean.sh
@@ -4,6 +4,7 @@
 #                           Kernel Build Script
 #
 ###############################################################################
+# 2014-09-23 Shev_t       : modified
 # 2011-10-24 effectivesky : modified
 # 2010-12-29 allydrop     : created
 ###############################################################################
@@ -12,6 +13,11 @@ make O=./obj/KERNEL_OBJ/ clean
 if [ -f ./zImage ]
 then
     rm ./zImage
+fi
+
+if [ -f ./new_any_kernel.zip ]
+then
+    rm ./new_any_kernel.zip
 fi
 
 if [ -f ./pvrsrvkm_sgx540_120.ko ]
