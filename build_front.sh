d86b505b690eb9dcb76d061fa9a8bb85f72fd2f1
ShevT <shev_t@mail.ru>
2014-09-24 14:46:28 +0400

    ver 002

--- build_front.sh
+++ build_front.sh
@@ -15,7 +15,7 @@
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=~/AndroidSources/linaro-4.9.2/bin/arm-eabi-
-export LOCALVERSION="-ok-001"
+export LOCALVERSION="-ok-002"
 export CCACHE_DIR=~/.ccache/kernel
 ccache -M 5G
 
--- build_viva.sh
+++ build_viva.sh
@@ -15,7 +15,7 @@
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=~/android/cm11/prebuilts/gcc/linux-x86/arm/linaro-4.8/bin/arm-eabi-
-export LOCALVERSION="-ok-001"
+export LOCALVERSION="-ok-002"
 export CCACHE_DIR=~/.ccache/kernel
 ccache -M 5G
 
