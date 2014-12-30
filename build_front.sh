4bef8d76e17bfcf66a602efe55b4c713912c5e39
ShevT <shev_t@mail.ru>
2014-12-30 08:41:34 +0300

    ver 014

--- build_front.sh
+++ build_front.sh
@@ -15,7 +15,7 @@
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=~/AndroidSources/linaro-4.9.3/bin/arm-eabi-
-export LOCALVERSION="-ok-014-test"
+export LOCALVERSION="-ok-014"
 export CCACHE_DIR=~/.ccache/kernel
 ccache -M 5G
 
